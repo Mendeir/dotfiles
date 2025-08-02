766c26c69b41085f81a5658c57c35751ba7a2d37
Adrianne <adriannemagracia.git@gmail.com>
2025-08-02 17:05:22 +0800

    Fix neovim configuration not pushed due to existing git config inside the nvim configuration

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.smarttab = true
+
+opt.scrolloff = 10
+opt.inccommand = "split"
+opt.ignorecase = true
+opt.breakindent = true
+opt.backspace = { "start", "eol", "indent" }
+
+vim.opt.wrap = true
+
+vim.opt.linebreak = true
+vim.opt.breakindent = true
